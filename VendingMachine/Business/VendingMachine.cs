48bcdaa8c4a1adafd14b757492c76dab7c7b1aca
Roman Gerzanych <roman.gerzanich@webforum.com>
2017-01-06 01:33:56 +0200

    Implemented current balance function

--- VendingMachine/Business/VendingMachine.cs
+++ VendingMachine/Business/VendingMachine.cs
@@ -10,7 +10,7 @@ namespace VendingMachine.Business
 {
 	public class VendingMachine : IVendingMachine
 	{
-		private Money _currentBallance;
+		private Money _currentBalance;
 		private readonly IVendingMachineStore _store;
 
 		public VendingMachine(IVendingMachineStore store) : this(store, new Money(0, 0))
@@ -21,7 +21,7 @@ namespace VendingMachine.Business
 		public VendingMachine(IVendingMachineStore store, Money currentBallance)
 		{
 			_store = store;
-			_currentBallance = currentBallance;
+			_currentBalance = currentBallance;
 		}
 
 		public string Manufacturer
@@ -36,7 +36,7 @@ namespace VendingMachine.Business
 		{
 			get
 			{
-				throw new NotImplementedException();
+				return _currentBalance;
 			}
 		}
 
@@ -57,10 +57,10 @@ namespace VendingMachine.Business
 
 		public void InsertCoin(Money amount)
 		{
-			var denominatedBallance = _currentBallance.Denominate();
+			var denominatedBallance = _currentBalance.Denominate();
 			var denominatedAmount = amount.Denominate();
 
-			_currentBallance.Parse(denominatedBallance + denominatedAmount);
+			_currentBalance.Parse(denominatedBallance + denominatedAmount);
 		}
 
 		public Money ReturnMoney()
@@ -71,11 +71,17 @@ namespace VendingMachine.Business
 		public Product BuyProduct(int productNumber)
 		{
 			var product = _store.GetProduct(productNumber);
+			var denominatedBallance = _currentBalance.Denominate();
+			var denominatedProductPrice = product.Price.Denominate();
 
-			if (_currentBallance.Denominate() < product.Price.Denominate())
+			if (denominatedBallance < denominatedProductPrice)
 			{
 				throw new InvalidOperationException("Not enough money to buy this product");
 			}
+
+			// update current ballance
+			var change = denominatedBallance - denominatedProductPrice;
+			_currentBalance = _currentBalance.Parse(change);
 
 			return product;
 		}
